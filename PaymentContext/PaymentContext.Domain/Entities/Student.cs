0f0d10740f8cb95a132d84b19f89c7b6815f4070
Paulo César Viana <paulo.cesar@implantainformatica.com.br>
2020-09-21 09:48:03 -0300

    Adicionando validação com Flunt

--- PaymentContext/PaymentContext.Domain/Entities/Student.cs
+++ PaymentContext/PaymentContext.Domain/Entities/Student.cs
@@ -8,18 +8,20 @@ namespace PaymentContext.Domain.Entities
     public class Student : Entity
     {
         private IList<Subscription> _subscriptions;
-        public Student(Name name, string lastName, Document document, string email)
+        public Student(Name name, string lastName, Document document, Email email)
         {
             Name = name;
             Document = document;
             Email = email;
             _subscriptions = new List<Subscription>();
+
+            AddNotifications(name, document, email);
         }
 
         public Name Name { get; private set; }
         public Document Document { get; private set; }
-        public string Email { get; private set; }
-        public string Address { get; private set; }
+        public Email Email { get; private set; }
+        public Address Address { get; private set; }
         public IReadOnlyCollection<Subscription> Subscriptions { 
             get 
             { 
